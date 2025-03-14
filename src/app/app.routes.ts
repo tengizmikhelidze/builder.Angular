beb250109cda56b6ac65043aa5a0194f8a246161
10GO <mixelidzet@gmail.com>
2025-03-15 02:24:26 +0400

    feat - folders structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home-page/home-page.routes').then(
+        (m) => m.routes
+      )
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: '**', redirectTo: 'home' }
+
+];
