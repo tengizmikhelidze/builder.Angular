849a009cbc6ebd4d0a4348e15374535cea913857
10GO <mixelidzet@gmail.com>
2025-03-17 23:42:39 +0400

    environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ import {Environment} from './environment.interface';
 
 export const environment: Environment = {
   environment: 'localhost',
-  apiUrl: 'http://173.249.17.223/api',
-  projectsUrl: 'http://173.249.17.223/api/projects'
+  apiUrl: 'http://localhost:3000/api',
+  projectsUrl: 'http://localhost:3000/api/projects'
 };
