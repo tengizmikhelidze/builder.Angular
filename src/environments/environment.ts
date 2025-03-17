1d151940b2056e1cf876b576bfb82f7179596d4e
10GO <mixelidzet@gmail.com>
2025-03-17 14:36:50 +0400

    environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import {Environment} from './environment.interface';
 
 export const environment: Environment = {
   environment: 'localhost',
-  apiUrl: 'https://api.10ggo.ge/api',
-  projectsUrl: 'https://api.10ggo.ge/api/projects'
+  apiUrl: 'https://10ggo.ge/api',
+  projectsUrl: 'https://10ggo.ge/api/projects'
 };
