06ade98a04f221478480887ef6dee4c310814f6e
10GO <mixelidzet@gmail.com>
2025-03-19 00:04:49 +0400

    speeddial section

--- src/app/features/contact-page/contact-page-first-section/contact-page-first-section.component.ts
+++ src/app/features/contact-page/contact-page-first-section/contact-page-first-section.component.ts
@@ -1,39 +1,10 @@
 import {Component} from '@angular/core';
-import {Button} from "primeng/button";
-import {SpeedDial} from "primeng/speeddial";
-import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-contact-page-first-section',
-  imports: [
-    Button,
-    SpeedDial,
-    NgClass,
-    NgStyle
-  ],
+  imports: [],
   templateUrl: './contact-page-first-section.component.html',
   styleUrls: ['./contact-page-first-section.component.scss', './contact-page-first-section.component.responsive.scss']
 })
 export class ContactPageFirstSectionComponent {
-  messageContacts = [
-    {
-      icon: "pi-facebook",
-      background: "#1877F2",
-      href: ''
-    },
-    {
-      icon: "pi-whatsapp",
-      background: "#25d366",
-      href: ''
-    }
-  ]
-
-  dialContacts = [
-    {
-      number: '(+995) 599 93 42 11'
-    },
-    {
-      number: '(+995) 555 93 42 11'
-    }
-  ]
 }
--- src/app/features/contact-page/contact-page.component.ts
+++ src/app/features/contact-page/contact-page.component.ts
@@ -1,16 +1,44 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from '../../shared/components';
 import {ContactPageFirstSectionComponent} from './contact-page-first-section/contact-page-first-section.component';
+import {Button} from "primeng/button";
+import {SpeedDial} from "primeng/speeddial";
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-contact-page',
   imports: [
     HeaderComponent,
-    ContactPageFirstSectionComponent
+    ContactPageFirstSectionComponent,
+    Button,
+    SpeedDial,
+    NgClass,
+    NgStyle
   ],
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss', './contact-page.component.responsive.scss']
 })
 export class ContactPageComponent {
 
+  messageContacts = [
+    {
+      icon: "pi-facebook",
+      background: "#1877F2",
+      href: ''
+    },
+    {
+      icon: "pi-whatsapp",
+      background: "#25d366",
+      href: ''
+    }
+  ]
+
+  dialContacts = [
+    {
+      number: '(+995) 599 93 42 11'
+    },
+    {
+      number: '(+995) 555 93 42 11'
+    }
+  ]
 }
