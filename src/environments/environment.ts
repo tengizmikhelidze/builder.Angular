8897cb8f4c818696232fdd93ad40026e3176e0d8
10GO <mixelidzet@gmail.com>
2025-03-19 12:13:48 +0400

    fix - config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import {Environment} from './environment.interface';
 
 export const environment: Environment = {
   environment: 'localhost',
-  apiUrl: 'https://10ggo.ge/api',
-  projectsUrl: 'https://10ggo.ge/api/projects'
+  apiUrl: 'https://buildnewidea.ge/api',
+  projectsUrl: 'https://buildnewidea.ge/api/projects'
 };
